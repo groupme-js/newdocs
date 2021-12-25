e5964fbc2b0c063353d9312071de8c87619acecc
Shadow <thebotoftheshadow@gmail.com>
2021-12-24 18:59:18 -0600

    main page content

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,23 @@
 import Head from "next/head"
-import styles from "../styles/Home.module.css"
 
 export default function Home() {
-  return (
-    <div>
-      <Head>
-        <title>Home | node-groupme</title>
-      </Head>
+    return (
+        <div>
+            <Head>
+                <title>Home | node-groupme</title>
+            </Head>
 
-      
-    </div>
-  )
+            <div className="columns is-mobile is-centered">
+                <div className="column is-half">
+                    <p className="text-center title">node-groupme</p>
+                    <p className="text-center subtitle">The only GroupMe API library for NodeJS that isn't a million years old.</p>
+                </div>
+            </div>
+            <div className="columns is-mobile is-centered">
+                <div className="column is-half">
+                    <p className="text-center">Inspired by discord.js, node-groupme aims to replace the obsolete groupme package with a modern, object-oriented, intuitive approach to interacting with the GroupMe API.</p>
+                </div>
+            </div>
+        </div>
+    )
 }
