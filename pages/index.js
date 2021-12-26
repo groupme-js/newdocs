63fbc03f409f0e1fda66a8d6fc230d6ad4cb8429
Shadow <thebotoftheshadow@gmail.com>
2021-12-25 20:17:21 -0600

    add some boxes

--- pages/index.js
+++ pages/index.js
@@ -7,15 +7,26 @@ export default function Home() {
                 <title>Home | node-groupme</title>
             </Head>
 
-            <div className="columns is-mobile is-centered">
-                <div className="column is-half">
-                    <p className="text-center title">node-groupme</p>
-                    <p className="text-center subtitle">The only GroupMe API library for NodeJS that isn&apos;t a million years old.</p>
-                </div>
-            </div>
-            <div className="columns is-mobile is-centered">
-                <div className="column is-half">
-                    <p className="text-center">Inspired by discord.js, node-groupme aims to replace the obsolete groupme package with a modern, object-oriented, intuitive approach to interacting with the GroupMe API.</p>
+            <div className="container">
+                <section className="section">
+                    <h1 className="title">node-groupme</h1>
+                    <h2 className="subtitle">The only GroupMe API library for NodeJS that isn&apos;t a million years old.</h2>
+                </section>
+                <div className="tile is-ancestor">
+                    <div className="tile is-parent">
+                        <div className="tile is-child box mx-2">
+                            <p className="title">Inspired by</p>
+                            <p className="subtitle">node-groupme was inspired by discord.js</p>
+                        </div>
+                        <div className="tile is-child box mx-2">
+                            <p className="title">Our goal</p>
+                            <p className="subtitle">Our goal is to replace the obsolete groupme package with a modern, object-oriented, intuitive approach to interacting with the GroupMe API.</p>
+                        </div>
+                        <div className="tile is-child box mx-2">
+                            <p className="title">Contribute</p>
+                            <p className="subtitle">This community-led effort is made possible by the unofficial GroupMe API Community Docs. Please contribute to the development of open-source applications by contributing to the community docs. Join the node-groupme server on Discord to discuss the API or ask any questions.</p>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
