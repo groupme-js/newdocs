bed69d75745d6116c2c95cb6bb71b81e7570043a
Shadow <thebotoftheshadow@gmail.com>
2021-12-26 17:28:40 -0600

    update meta tags

--- pages/_document.js
+++ pages/_document.js
@@ -1,32 +1,38 @@
 import Document, { Html, Head, Main, NextScript } from "next/document"
 
 class MyDocument extends Document {
-  static async getInitialProps(ctx) {
-    const initialProps = await Document.getInitialProps(ctx)
-    return { ...initialProps }
-  }
+    static async getInitialProps(ctx) {
+        const initialProps = await Document.getInitialProps(ctx)
+        return { ...initialProps }
+    }
 
-  render() {
-    return (
-      <Html>
-        <Head>
-          <meta charSet="utf-8" />
+    render() {
+        return (
+            <Html>
+                <Head>
+                    <meta charSet="utf-8" />
 
-          <meta name="theme-color" content="#70503C" />
-          <meta property="description" content="Inspired by discord.js, node-groupme aims to replace the obsolete groupme package with a modern, object-oriented, intuitive approach to interacting with the GroupMe API." />
-          <meta property="og:title" content="node-groupme" />
-          <meta property="og:theme-color" content="#70503C" />
-          <meta property="og:url" content="https://groupme.js.org" />
-          <meta property="od:description" content="Inspired by discord.js, node-groupme aims to replace the obsolete groupme package with a modern, object-oriented, intuitive approach to interacting with the GroupMe API." />
-          <script async src="https://kit.fontawesome.com/114d30bb2c.js" crossOrigin="anonymous"></script>
-        </Head>
-        <body className="">
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
-    )
-  }
+                    <meta name="theme-color" content="#4EACEA" />
+                    <meta property="description" content="The only GroupMe API library for NodeJS that isn't a million years old." />
+                    <meta property="og:title" content="node-groupme" />
+                    <meta property="og:theme-color" content="#4EACEA" />
+                    <meta property="og:url" content="https://groupme.js.org" />
+                    <meta property="od:description" content="The only GroupMe API library for NodeJS that isn't a million years old." />
+                    <script async src="https://kit.fontawesome.com/114d30bb2c.js" crossOrigin="anonymous"></script>
+                    <meta name="title" content="node-groupme" />
+                    <meta name="description" content="The only GroupMe API library for NodeJS that isn't a million years old." />
+                    <meta name="keywords" content="groupme,api,node-groupme,groupme.js" />
+                    <meta name="robots" content="index, follow" />
+                    <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
+                    <meta name="language" content="English" />
+                </Head>
+                <body className="">
+                    <Main />
+                    <NextScript />
+                </body>
+            </Html>
+        )
+    }
 }
 
 export default MyDocument
